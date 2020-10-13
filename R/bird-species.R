2e80339e63658411b9198ec869fa4c64c80881e7
Daniel Falbel <dfalbel@gmail.com>
2020-10-13 11:11:07 -0300

    fix path to board

--- R/bird-species.R
+++ R/bird-species.R
@@ -5,7 +5,7 @@ kaggle_download <- function(name, token = NULL) {
     file <- pins::pin_get(board = "kaggle", name,
                           extract = FALSE)
   } else if (!is.null(token)) {
-    pins::board_register_kaggle(name=name, token = token,
+    pins::board_register_kaggle(name="torchdatasets-kaggle", token = token,
                                 cache = tempfile(pattern = "dir"))
     on.exit({pins::board_deregister("torchdatasets-kaggle")}, add = TRUE)
     file <- pins::pin_get(name,
